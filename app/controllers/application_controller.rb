4bd4cb5f01bf14cd9a7c7a771a0d2b78e820e9a1
romanolivi <romano_livi@outlook.com>
2021-03-03 16:27:24 -0500

    filled out user and application controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,1 +1,11 @@
+class ApplicationController < ActionController::Base
+    helper_method :current_user
 
+    def home 
+        
+    end
+
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+    before_action :require_login
+    skip_before_action :require_login, only: [:new, :create]
+
+    def new 
+        @user = User.new 
+    end 
+
+    def create 
+        @user = User.create(user_params)
+
+        if @user.save 
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else 
+            redirect_to '/'
+        end
+    end 
+
+    def show 
+        @user = current_user
+    end 
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
+    def require_login 
+        redirect_to '/' unless session.include? :user_id
+    end
+end
