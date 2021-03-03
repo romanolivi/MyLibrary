4837bc88ada8e607f9d60264cfc92213b035861e
romanolivi <romano_livi@outlook.com>
2021-03-03 16:24:15 -0500

    Created tables, models, and controllers

---
+++ db/migrate/20210303202125_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.string :genre
+      t.string :author
+      t.integer :page_count
+
+      t.timestamps
+    end
+  end
+end
