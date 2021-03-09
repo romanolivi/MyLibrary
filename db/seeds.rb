4f1e6dbdcc23c74e5643fbc2b3df3a79c6936ee8
romanolivi <romano_livi@outlook.com>
2021-03-09 11:54:12 -0500

    Added error messages to forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,9 @@ four = Book.create(name: 'The 4-Hour Workweek', author: 'Tim Ferris', genre: 'Se
 shadow = Book.create(name: 'Shadow of the Wind', author: 'Carlos Ruiz Zafon', genre: 'Mystery', page_count: 487)
 gatsby = Book.create(name: 'The Great Gatsby', author: 'F. Scott Fitzgerald', genre: 'Tragedy', page_count: 218)
 ulysses = Book.create(name: 'Ulysses', author: 'James Joyce', genre: 'Modernist', page_count: 730)
+
+a = Book.create(name: 'Don Quixote', author: 'Miguel De Cervantes', genre: 'Adventure', page_count: 863)
+b = Book.create(name: '100 Years of Solitude', author: 'Gabriel Garcia Marquez', genre: 'Fantasy', page_count: 440)
+c = Book.create(name: 'Hatchet', author: 'Gary Paulsen', genre: 'Adventure', page_count: 190)
+d = Book.create(name: 'Harry Potter', author: 'J.K Rowling', genre: 'Fantasy', page_count: 300)
+e = Book.create(name: 'Macbeth', author: 'William Shakespear', genre: 'Tragedy', page_count: 110)
